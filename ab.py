588a9611f2c8bb337a928b2b18866596fad6737b
liormw <102317697+liormw@users.noreply.github.com>
2022-08-18 09:06:36 +0300

    Update ab.py

--- ab.py
+++ ab.py
@@ -5,39 +5,3 @@ subprocess.Popen('/bin/echo', shell=True)
 subprocess.Popen('/bin/echo', shell=True)
 
 subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
